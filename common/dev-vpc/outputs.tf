58cb36bb21f46c1920004d1575ebdffc65840847
JeonghwanSa <jeonghwansa@jobis.co>
2024-08-01 18:16:17 +0900

    INFRA-4315 add private hosted zone, endpoint records

--- common/dev-vpc/outputs.tf
+++ common/dev-vpc/outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_peering_id" {
   value = aws_vpc_peering_connection.data-ai-us-west-2-peer.id
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
--- common/uwst2-vpc/main.tf
+++ common/uwst2-vpc/main.tf
@@ -65,17 +65,62 @@ resource "aws_route" "stg-route" {
   vpc_peering_connection_id = data.terraform_remote_state.stg-vpc.outputs.vpc_peering_id
 }
 
-resource "aws_security_group" "bedrock_runtime-sg" {
+## bedrock endpoint security group
+resource "aws_security_group" "bedrock-endpoint-sg" {
   vpc_id = module.vpc.vpc_id
 }
 
+resource "aws_vpc_security_group_ingress_rule" "bedrock-endpoint-sg-https-ingress-dev" {
+  security_group_id = aws_security_group.bedrock-endpoint-sg.id
+
+  cidr_ipv4   = "10.50.0.0/16"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "bedrock-endpoint-sg-https-ingress-stg" {
+  security_group_id = aws_security_group.bedrock-endpoint-sg.id
+
+  cidr_ipv4   = "10.51.0.0/16"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+## Private Hosted Zone
+resource "aws_route53_zone" "stg-data-ai-bedrock-jobis-internal" {
+  name = "stg.data-ai-bedrock.jobis.internal"
+
+  vpc {
+    vpc_id = module.vpc.vpc_id
+  }
+
+  lifecycle {
+    ignore_changes = [vpc]
+  }
+}
+
+resource "aws_route53_zone_association" "dev-vpc" {
+  zone_id    = aws_route53_zone.stg-data-ai-bedrock-jobis-internal.zone_id
+  vpc_id     = data.terraform_remote_state.dev-vpc.outputs.vpc_id
+  vpc_region = "ap-northeast-2"
+}
+
+resource "aws_route53_zone_association" "stg-vpc" {
+  zone_id    = aws_route53_zone.stg-data-ai-bedrock-jobis-internal.zone_id
+  vpc_id     = data.terraform_remote_state.stg-vpc.outputs.vpc_id
+  vpc_region = "ap-northeast-2"
+}
+
+## Bedrock Runtime VPC Endpoint
 resource "aws_vpc_endpoint" "bedrock-runtime" {
   vpc_id            = module.vpc.vpc_id
   service_name      = "com.amazonaws.us-west-2.bedrock-runtime"
   vpc_endpoint_type = "Interface"
 
   security_group_ids = [
-    aws_security_group.bedrock_runtime-sg.id,
+    aws_security_group.bedrock-endpoint-sg.id,
   ]
 
   subnet_ids = module.vpc.private_subnets
@@ -83,3 +128,95 @@ resource "aws_vpc_endpoint" "bedrock-runtime" {
   private_dns_enabled = false
 }
 
+resource "aws_route53_record" "bedrock-runtime" {
+  zone_id = aws_route53_zone.stg-data-ai-bedrock-jobis-internal.zone_id
+  name    = "bedrock-runtime.stg.data-ai-bedrock.jobis.internal"
+  type    = "A"
+
+  alias {
+    name                   = aws_vpc_endpoint.bedrock-runtime.dns_entry[0].dns_name
+    zone_id                = aws_vpc_endpoint.bedrock-runtime.dns_entry[0].hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+## Bedrock VPC Endpoint
+resource "aws_vpc_endpoint" "bedrock" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.us-west-2.bedrock"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.bedrock-endpoint-sg.id,
+  ]
+
+  subnet_ids = module.vpc.private_subnets
+
+  private_dns_enabled = false
+}
+
+resource "aws_route53_record" "bedrock" {
+  zone_id = aws_route53_zone.stg-data-ai-bedrock-jobis-internal.zone_id
+  name    = "bedrock.stg.data-ai-bedrock.jobis.internal"
+  type    = "A"
+
+  alias {
+    name                   = aws_vpc_endpoint.bedrock.dns_entry[0].dns_name
+    zone_id                = aws_vpc_endpoint.bedrock.dns_entry[0].hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+## Bedrock Agent VPC Endpoint
+resource "aws_vpc_endpoint" "bedrock-agent" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.us-west-2.bedrock-agent"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.bedrock-endpoint-sg.id,
+  ]
+
+  subnet_ids = module.vpc.private_subnets
+
+  private_dns_enabled = false
+}
+
+resource "aws_route53_record" "bedrock-agent" {
+  zone_id = aws_route53_zone.stg-data-ai-bedrock-jobis-internal.zone_id
+  name    = "bedrock-agent.stg.data-ai-bedrock.jobis.internal"
+  type    = "A"
+
+  alias {
+    name                   = aws_vpc_endpoint.bedrock-agent.dns_entry[0].dns_name
+    zone_id                = aws_vpc_endpoint.bedrock-agent.dns_entry[0].hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+## Bedrock Agent Runtime VPC Endpoint
+resource "aws_vpc_endpoint" "bedrock-agent-runtime" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.us-west-2.bedrock-agent-runtime"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.bedrock-endpoint-sg.id,
+  ]
+
+  subnet_ids = module.vpc.private_subnets
+
+  private_dns_enabled = false
+}
+
+resource "aws_route53_record" "bedrock-agent-runtime" {
+  zone_id = aws_route53_zone.stg-data-ai-bedrock-jobis-internal.zone_id
+  name    = "bedrock-agent-runtime.stg.data-ai-bedrock.jobis.internal"
+  type    = "A"
+
+  alias {
+    name                   = aws_vpc_endpoint.bedrock-agent-runtime.dns_entry[0].dns_name
+    zone_id                = aws_vpc_endpoint.bedrock-agent-runtime.dns_entry[0].hosted_zone_id
+    evaluate_target_health = true
+  }
+}
